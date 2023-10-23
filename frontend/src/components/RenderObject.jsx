db48f93d4254e74511fc11a1b24033b12588c548
danielhan60903 <danielhan60903@gmail.com>
2023-10-23 14:12:59 +0900

    save image functionality

--- frontend/src/components/RenderObject.jsx
+++ frontend/src/components/RenderObject.jsx
@@ -1,17 +1,11 @@
-import React, { useState, Suspense } from "react";
+import React, { useState, Suspense, useRef } from "react";
 import { Canvas } from "@react-three/fiber";
 import { Html, OrbitControls, Stage } from "@react-three/drei";
 import Model from "./renderHelp/Model";
 import Lighting from "./renderHelp/Lighting";
 import "../styles/RenderObject.css";
 
-/* 
-Component to render Three.js canvas.
-    - modelUrl: filePath of desired glb model to be rendered 
-*/
 const RenderObject = ({ modelUrl }) => {
-
-  // settings consts for background color and lightType
   const [backgroundColor, setBackgroundColor] = useState("#7E8A6A");
   const [lightType, setLightType] = useState("ambient");
 
@@ -23,9 +17,24 @@ const RenderObject = ({ modelUrl }) => {
   const isLightHighlighted = (type) =>
     lightType === type ? "highlighted-button" : "";
 
-  /*
+  const canvasRef = useRef();
 
-  */
+  const handleDownloadImage = () => {
+    const canvas = canvasRef.current;
+    if (!canvas) return;
+
+    canvas.toBlob((blob) => {
+      if (blob) {
+        const url = URL.createObjectURL(blob);
+        const a = document.createElement("a");
+        a.href = url;
+        a.download = "rendered_image.jpg";
+        a.click();
+        URL.revokeObjectURL(url);
+      }
+    });
+  };
+
   const getLight = () => {
     switch (lightType) {
       case "directional":
@@ -39,19 +48,20 @@ const RenderObject = ({ modelUrl }) => {
     }
   };
 
+
   return (
     <div>
       <div className="filename-label">{filename}</div>
       <div className="container">
         <div className="renderBox">
-
-          {/* Three.js Canvas for 3D model */}
           <Canvas
             dpr={[1, 2]}
             shadows={{}}
             camera={{ position: [0, 50, 50], fov: 45 }}
-            style={{ backgroundColor }}
+            ref={canvasRef}
+            gl={{ preserveDrawingBuffer: true }}
           >
+            <color attach="background" args={[backgroundColor]} />
             <OrbitControls></OrbitControls>
             <Suspense fallback={<Html center>Loading...</Html>}>
               <Stage environment={"sunset"}>
@@ -76,6 +86,9 @@ const RenderObject = ({ modelUrl }) => {
             setLightType={setLightType}
             isLightHighlighted={isLightHighlighted}
           />
+          <br />
+          <button onClick={handleDownloadImage}>Download Image</button>{" "}
+          {/* Step 4: Add the download button */}
         </div>
       </div>
     </div>
